d7b00ac34eb2c410ba69c9e6e1db62d8740d35b6
Ayushmaan Singh <73950283+Amadeus017@users.noreply.github.com>
2022-11-25 01:57:43 +0530

    created test1

---
+++ test1.py
@@ -0,0 +1,16 @@
+!/usr/bin/python3
+import unittest
+
+from p1 import getSum
+
+class test(unittest.Testcase):
+  def test_list_int(self):
+    """
+    Test case 1
+    """
+    data = [17, 18]
+    ans = getSum(data)
+    self.assertEqual(ans, 35)
+    
+if __name__ == '__main__':
+  unittest.main()
