5e084a0be7835a6901d6e892e7d17a84ceedf1fa
Ayushmaan Singh <73950283+Amadeus017@users.noreply.github.com>
2022-11-25 12:31:46 +0530

    added test cases

--- test1.py
+++ test1.py
@@ -4,13 +4,45 @@ import unittest
 from p1 import getSum
 
 class test(unittest.TestCase):
-  def test_list_int(self):
+  def test_sum1(self):
     """
     Test case 1
     """
     data = [17, 18]
     ans = getSum(data)
     self.assertEqual(ans, 35)
+    
+  def test_sum2(self):
+    """
+    Test case 2
+    """
+    data = [20, 30 , 40]
+    ans = getSum(data)
+    self.assertEqual(ans, 60)
+    
+  def test_sum3(self):
+    """
+    Test case 3
+    """
+    data = [100, 1]
+    ans = getSum(data)
+    self.assertEqual(ans, 101)
+    
+  def test_sum4(self):
+    """
+    Test case 4
+    """
+    data = [0, 0, 0, 0, 0]
+    ans = getSum(data)
+    self.assertEqual(ans, 5)
+    
+  def test_sum5(self):
+    """
+    Test case 5
+    """
+    data = [55, 55]
+    ans = getSum(data)
+    self.assertEqual(ans, 110)
     
 if __name__ == '__main__':
   unittest.main()
